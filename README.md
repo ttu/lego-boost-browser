df2be5d62d2f86776449cf779a0fea4aaff4667d
Tomi Tuhkanen <tomi.tuhkanen@iki.fi>
2018-12-04 23:28:14 +0200

    Add AI methods

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 
 > Work in progress
 
-Command Lego Boost from browser
+Control Lego Boost staigth from the browser without any installations.
+
+Uses [Web Bluetooth API](https://developer.mozilla.org/en-US/docs/Web/API/Web_Bluetooth_API). Works with Chrome and Opera.
 
 ## Start
 
@@ -11,15 +13,30 @@ $ npm run build:browser
 # Open index.html
 ```
 
-#### Notes / Links
+### Notes
+
+`hubAsync.ts` is copy from [movehub-async.js](https://github.com/ttu/node-movehub-async/blob/master/movehub-async.js).
+
+`ai`-implementation is copy from [lego-boost-ai](https://github.com/ttu/lego-boost-ai).
+
+#### Links
 
 * https://developers.google.com/web/updates/2015/07/interact-with-ble-devices-on-the-web
 * https://www.typescriptlang.org/docs/handbook/migrating-from-javascript.html
 * https://www.typescriptlang.org/docs/handbook/integrating-with-build-tools.html#browserify
 
+
+###### Testers
+
+* https://googlechrome.github.io/samples/web-bluetooth/read-characteristic-value-changed.html
+* https://googlechrome.github.io/samples/web-bluetooth/notifications.html
+* https://googlechrome.github.io/samples/web-bluetooth/notifications-async-await.html
+
 ## Credits
 
 Node module for controlling Lego Boost: https://github.com/hobbyquaker/node-movehub
+
+Async implementation of Node module: https://github.com/ttu/node-movehub-async
 
 Angular implementation: https://github.com/BenjaminDobler/ng-lego-boost
 
---
+++ src/ai/hub-control.js
@@ -0,0 +1,90 @@
+const manual = require('./states/manual');
+const { stop, back, drive, turn, seek } = require('./states/ai');
+
+class HubControl {
+  constructor(deviceInfo, controlData) {
+    this.hub = null;
+    this.device = deviceInfo;
+    this.control = controlData;
+    this.prevControl = { ...this.control };
+
+    this.states = {
+      Turn: turn.bind(this),
+      Drive: drive.bind(this),
+      Stop: stop.bind(this),
+      Back: back.bind(this),
+      Manual: manual.bind(this),
+      Seek: seek.bind(this)
+    };
+
+    this.currentState = this.states['Drive'];
+  }
+
+  async start(hub) {
+    this.hub = hub;
+    this.device.connected = true;
+
+    this.hub.emitter.on('error', err => {
+      this.device.err = err;
+    });
+
+    this.hub.emitter.on('disconnect', () => {
+      this.device.connected = false;
+    });
+
+    this.hub.emitter.on('distance', distance => {
+      this.device.distance = distance;
+    });
+
+    this.hub.emitter.on('rssi', rssi => {
+      this.device.rssi = rssi;
+    });
+
+    this.hub.emitter.on('port', portObject => {
+      const { port, action } = portObject;
+      this.device.ports[port].action = action;
+    });
+
+    this.hub.emitter.on('color', color => {
+      this.device.color = color;
+    });
+
+    this.hub.emitter.on('tilt', tilt => {
+      const { roll, pitch } = tilt;
+      this.device.tilt.roll = roll;
+      this.device.tilt.pitch = pitch;
+    });
+
+    this.hub.emitter.on('rotation', rotation => {
+      const { port, angle } = rotation;
+      this.device.ports[port].angle = angle;
+    });
+
+    await this.hub.ledAsync('red');
+    await this.hub.ledAsync('yellow');
+    await this.hub.ledAsync('green');
+  }
+
+  async disconnect() {
+    if (this.device.connected) {
+      await this.hub.disconnectAsync();
+    }
+  }
+
+  setNextState(state) {
+    this.control.driveInput = null;
+    this.control.state = state;
+    this.currentState = this.states[state];
+  }
+
+  update() {
+    this.currentState();
+
+    // TODO: Deep clone
+    this.prevControl = { ...this.control };
+    this.prevControl.tilt = { ...this.control.tilt };
+    this.prevDevice = { ...this.device };
+  }
+}
+
+export { HubControl }
